2e6f36c4e00fa17eafb35917b8fe70ba09d7eacf
orisabiyi <orisabiyidavid@gmail.com>
2025-03-29 18:05:10 +0100

    Refactor layout and index components for improved structure and readability

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,9 @@
-import "./global.css"
+import "./global.css";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      // style={{
-      //   flex: 1,
-      //   justifyContent: "center",
-      //   alignItems: "center",
-      // }}
-      className="flex-1 flex justify-center items-center"
-    >
+    <View className="flex-1 flex justify-center items-center">
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
